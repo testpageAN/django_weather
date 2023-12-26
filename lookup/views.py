90b0dec57eeb64a5b620fd4c618977e55f9d5beb
testpageAN <alexis.nikolopoulos@hotmail.com>
2023-12-26 22:50:28 +0200

    gitignore added 26/12/2023-rev-3

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=25&API_KEY=BCCECF5B-A827-4931-8E0D-AA3DDF3A7273")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render(request, 'home.html', {'api': api})
 
 
 def about(request):
